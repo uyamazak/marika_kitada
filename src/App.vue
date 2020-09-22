197d2097ed883d2cae9050fc6c9159c373bc9cae
uyamazak <yu.yamazaki85@gmail.com>
2020-09-22 12:33:28 +0900

    add recorder

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
   <div id="app">
-    <v-stage :config="configKonva">
-      <v-layer ref="body">
-        <v-image :config="{
-          image: bodyImage,
-          scaleX:0.2,
-          scaleY:0.2,
-        }"/>
-      </v-layer>
-
-        
-        <v-layer ref="eyes">
-          <v-image :config="{
+    <v-stage :config="configKonva" ref='stage' id='stage'>
+        <v-layer ref='marika' id='marika'>
+          <v-group ref="marikaGroup">
+            <v-image 
+            ref='eyes'
+            :config="{
             image: eyesImage,
-            scaleX:0.2,
-            scaleY:0.2,
-            x:20,
+            scaleX:allScale,
+            scaleY:allScale,
+            x: eyeX,
+            y: eyeY,
           }"/>
+          <v-image 
+          ref='body'
+          :config="{
+            image: bodyImage,
+            scaleX:allScale,
+            scaleY:allScale,
+          }"
+          />
+
+
+           </v-group>
         </v-layer>
-
-
     </v-stage>
+    <button @click='start'>start</button>
+    <button @click='stop'>stop</button>
+    <a v-if='movieData' :href='movieData' download='movie.webm'>download</a>
   </div>
 </template>
 
@@ -46,35 +53,48 @@ export default {
       configKonva: {
         width: 1920,
         height: 1080,
+        background: 'red',
       },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 70,
-        fill: "black",
-        stroke: "black",
-        strokeWidth: 4
-      }
+      eyeX: 5,
+      eyeY: 1,
+      allScale: 0.2,
+      stream: null,
+      recorder: null,
+      movieData: null,
     };
   },
+  methods:{
+    start(){
+      console.log('start')
+      this.movieData = null
+      this.recorder.start()
+      console.log(this.recorder)
+    },
+    stop(){
+      console.log('stop')
+      this.recorder.stop()
+      console.log(this.recorder)
+    },
+  },
   async mounted() {
-    /*
-    const bodyImg = new window.Image();
-    bodyImg.src = "/images/karada.png";
-    bodyImg.onload = () => {
-      this.bodyImage = bodyImg;
-      console.log(bodyImg.width, bodyImg.height)
-    };
-    const eyesImg = new window.Image();
-    eyesImg.src = "/images/hitomi.png";
-    eyesImg.onload = () => {
-      this.eyesImage = eyesImg;
-    };
-    */
-    
-   this.bodyImage = await addImageProcess('/images/karada.png')
-   this.eyesImage = await addImageProcess('/images/hitomi.png')
-  
+    this.eyesImage = await addImageProcess('/images/hitomi.png')
+    this.bodyImage = await addImageProcess('/images/karada.png')
+    this.$refs.eyes.getNode().moveToBottom()
+    this.stream = document.getElementsByTagName('canvas')[0].captureStream()
+    this.recorder = new MediaRecorder(this.stream, {mimeType:'video/webm;codecs=vp9'});
+    this.recorder.ondataavailable = (e) => {
+      console.log('ondataavailable')
+      const videoBlob = new Blob([e.data], { type: e.data.type })
+      this.movieData = window.URL.createObjectURL(videoBlob)
+    }
+    console.log(this.stream)
+    /**/
+  },
+  created() {
+    setInterval(()=>{
+      this.eyeX *=-1
+      this.eyeY *=-1
+    }, 100)
   }
 }
 </script>
